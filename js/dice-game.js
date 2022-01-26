b3836fe71b8035383e68113d4e366680591ed0d2
kenjulian <kenneth.julian@yahoo.com>
2022-01-26 18:28:50 -0500

    Dice game

---
+++ js/dice-game.js
@@ -0,0 +1,19 @@
+function rollDice() {
+    let goldCoins = 0;
+
+    for (let i = 0; i < 10; i++) {
+        const roll = Math.floor(Math.random() * 6) + 1;
+        alert("You roll a " + roll + ".");
+        if (roll === 1) {
+            alert("Game over, no more rolls!");
+            break;//exits loop
+        }
+        if (roll < 5) {
+            continue;//skips loop
+        }
+        alert("Congratulations, you win " + roll + " gold coins!");
+        goldCoins += roll;
+
+    }
+    alert("You have won a total of " + goldCoins + " gold coins!")
+}
