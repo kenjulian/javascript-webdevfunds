02f1d4393d3d3b533637bf7286ca7d821e92214c
kenjulian <kenneth.julian@yahoo.com>
2022-01-26 21:17:00 -0500

    added extra rule for rolling a 4

--- js/dice-game.js
+++ js/dice-game.js
@@ -1,16 +1,31 @@
 function rollDice() {
     let goldCoins = 0;
+    console.log("hello")
 
     for (let i = 0; i < 10; i++) {
         const roll = Math.floor(Math.random() * 6) + 1;
         alert("You roll a " + roll + ".");
+        console.log(roll);
+
         if (roll === 1) {
             alert("Game over, no more rolls!");
             break;//exits loop
         }
-        if (roll < 5) {
+
+        if (roll < 4) {
             continue;//skips loop
         }
+
+        if (roll === 4) {
+            console.log("I am 4");
+            if (goldCoins) {
+                --goldCoins;
+            }
+            
+            console.log(goldCoins);
+            continue;
+        }
+
         goldCoins += roll;
         alert("Congratulations, you win " + roll + " gold coins!\n\nTotal coins: " + goldCoins);
 
