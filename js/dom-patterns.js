ad168c2defbc95f53b68a50cdbe002d273920f13
kenjulian <kenneth.julian@yahoo.com>
2022-01-28 19:46:53 -0500

    created html for dom pattern

---
+++ js/dom-patterns.js
@@ -0,0 +1,12 @@
+function showPattern() {
+    const colorsArr = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+    
+    let topPosition = 25;
+    let leftPosition = 25;
+    let width = 500;
+    let height = 500;
+
+    while (width > 50) {
+        const randomColorIdx = Math.floor(Math.random() * colorsArr.length)
+    }
+}
