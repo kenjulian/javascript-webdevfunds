79fa6b0c5972ad3004f94a0b640b8a5d71500d5d
kenjulian <kenneth.julian@yahoo.com>
2022-01-28 20:22:46 -0500

    pattern function created

--- js/dom-patterns.js
+++ js/dom-patterns.js
@@ -7,6 +7,22 @@ function showPattern() {
     let height = 500;
 
     while (width > 50) {
-        const randomColorIdx = Math.floor(Math.random() * colorsArr.length)
+        const randomColorIdx = Math.floor(Math.random() * colorsArr.length);
+        const newDiv = document.createElement("div");
+
+        newDiv.style.top = topPosition + 'px';
+        newDiv.style.left = leftPosition + 'px';
+        newDiv.style.width = width + 'px';
+        newDiv.style.height = height + 'px';
+        newDiv.style.background = colorsArr[randomColorIdx];
+
+        document.body.appendChild(newDiv);//creates very first square(biggest square)
+        topPosition += 10;//move 10px down from top
+        leftPosition += 10;//move 10px to the right
+        width -= 20;
+        height -= 20;
+
+        //a smaller and nested square should appear in every loop
+
     }
 }
